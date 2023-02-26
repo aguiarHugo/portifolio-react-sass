42c416f0293451dbebef91363ed2bf5d9eabb595
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-26 14:01:28 -0300

    Styled

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+
+const About = () => {
+  return (
+    <section className="about">
+      <h2>Sobre</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe,
+        nulla consequatur debitis harum expedita voluptas ipsum aspernatur 
+        assumenda dolorem distinctio quod doloremque modi minus
+        quidem architecto provident dolorum maxime corporis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe,
+        nulla consequatur debitis harum expedita voluptas ipsum aspernatur 
+        assumenda dolorem distinctio quod doloremque modi minus
+        quidem architecto provident dolorum maxime corporis.
+      </p>
+
+    </section>
+  )
+}
+
+export default About
--- src/containers/MainContent.jsx
+++ src/containers/MainContent.jsx
@@ -1,9 +1,14 @@
+import About from '../components/About'
+import Projects from '../components/Projects'
+import Technologies from '../components/Technologies'
 import '../styles/containers/maincontent.sass'
 
 const MainContent = () => {
   return (
-    <main id="main_content">
-      MainContent
+    <main id="main-content">
+      <About />
+      <Technologies />
+      <Projects />
     </main>
   )
 }
