7641957d056bba9596b6c2d7df1fcfae8f5e03aa
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-26 10:18:25 -0300

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import ProfilePic from '../assets/eu-bg-dark.png'
+
+import '../styles/components/sidebar.sass'
+
+
+const Sidebar = () => {
+  return (
+    <aside id="sidebar">
+      <p>img</p>
+      <div className="title">Desenvolvedor</div>
+      <p>redes sociais</p>
+      <p>informações de contato</p>
+      <a href="" className="btn">Baixe meu CV</a>
+    </aside>
+  )
+}
+
+export default Sidebar
