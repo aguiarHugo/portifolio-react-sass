950840fc4d272fda998090b53d4e0c25466f9bf7
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-26 14:43:38 -0300

    Styled

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className="projects">
+        <h2>Projetos</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          Consequuntur soluta reiciendis doloremque facilis quibusdam a
+          spernatur facere quaerat, nam, reprehenderit architecto, 
+          deserunt temporibus quidem saepequi quisquam repellendus dignissimos similique iure!
+        </p>
+        <a href="" className="btn" style={{ width: '200px', textAlign: 'center'}}>Ver Projetos</a>
+    </section>
   )
 }
 
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import {
+  DiHtml5,
+  DiCss3,
+  DiJsBadge,
+  DiNodejsSmall,
+  DiReact
+} from 'react-icons/di'
 
+const technologiesIcons = [
+  {id: 'html', name: "HTML5", icon: <DiHtml5 />},
+  {id: 'css', name: "CSS3", icon: <DiCss3 />},
+  {id: 'js', name: "JavaScript", icon: <DiJsBadge />},
+  {id: 'react', name: "React", icon: <DiReact />},
+  {id: 'node', name: "Node.js", icon: <DiNodejsSmall />}
+]
+
+import '../styles/components/technologies.sass'
 const Technologies = () => {
   return (
-    <div>Technologies</div>
+    <section className="techs">
+      <h2>Tecnologias</h2>
+      <div className="techs-grid">
+        {technologiesIcons.map((tech) => (
+          <div className="techs-card" id={tech.id} key={tech.id}>
+            {tech.icon}
+            <div className="techs-info">
+              <h3>{tech.name}</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tempora</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
